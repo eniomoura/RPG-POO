fdb18c201f6d3d65e42e70fb58e221f98cf0db8f
Enio <Enio@192.168.1.51>
2014-06-30 23:50:53 -0300

    Alterado o nome da variável "playerstats" para permitir possível futura reutilização do painel.

--- RPG-POO/src/GUIJogo/PainelChar.java
+++ RPG-POO/src/GUIJogo/PainelChar.java
@@ -20,25 +20,25 @@ public class PainelChar extends JPanel{
     public static JLabel sexo=new JLabel(InfoChar.sexo);
     public static JLabel forca=new JLabel("Força: "+Integer.toString(InfoChar.forca));
     public static JLabel inteligencia=new JLabel("Inteligência: "+Integer.toString(InfoChar.inteligencia));
-    static JPanel playerstats=new JPanel(new GridLayout(0, 1));
+    static JPanel statspanel=new JPanel(new GridLayout(0, 1));
 
     PainelChar(){
         //CONFIGURAÇÃO BÁSICA DO PAINEL
         super(new BorderLayout());
         setBorder(BorderFactory.createLineBorder(Color.black));
-        playerstats.setVisible(false);
+        statspanel.setVisible(false);
         player.setVisible(false);
         add(player, BorderLayout.PAGE_START);
-        add(playerstats, BorderLayout.LINE_START);
-        playerstats.add(sexo);
-        playerstats.add(forca);
-        playerstats.add(inteligencia);
+        add(statspanel, BorderLayout.LINE_START);
+        statspanel.add(sexo);
+        statspanel.add(forca);
+        statspanel.add(inteligencia);
     }
 
     public static void atualizar(){ //Atualiza o painel
         if(Historia.parteDaHistoria==0){ //Novo Jogo. Nome, sexo e classe ainda indefinidos.
             player.setVisible(false);
-            playerstats.setVisible(false);
+            statspanel.setVisible(false);
         }else if(Historia.parteDaHistoria==1){ //Nome definido.
             player.setText("Nome: "+InfoChar.nome);
             player.setVisible(true);
@@ -47,13 +47,13 @@ public class PainelChar extends JPanel{
             sexo.setText("Sexo: "+InfoChar.sexo);
             forca.setText("Força: "+Integer.toString(InfoChar.forca));
             inteligencia.setText("Inteligência: "+Integer.toString(InfoChar.inteligencia));
-            playerstats.setVisible(true);
+            statspanel.setVisible(true);
         }else{ //Ficha de personagem já construída. Todos os dados dispostsos.
             player.setText("Nome: "+InfoChar.nome+("Masculino".equals(InfoChar.sexo) ? ", o " : ", a ")+InfoChar.classe);
             sexo.setText("Sexo: "+InfoChar.sexo);
             forca.setText("Força: "+Integer.toString(InfoChar.forca));
             inteligencia.setText("Inteligência: "+Integer.toString(InfoChar.inteligencia));
-            playerstats.setVisible(true);
+            statspanel.setVisible(true);
         }
     }
 
