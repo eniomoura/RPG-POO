491baad808a4e3695c4b2bbc9b94509f84d8abcb
Verde <Verde@Verde-PC>
2014-07-05 02:41:11 -0300

    Inutilizei o sistema de decisão, e fiz tudo na base de cenas individuais.
    Os caminhos e lutas estão todos definidos aí.

--- RPG-POO/src/GUIJogo/PainelJogo.java
+++ RPG-POO/src/GUIJogo/PainelJogo.java
@@ -72,22 +72,97 @@ public class PainelJogo extends JPanel{
             button1.setVisible(true);
             button2.setVisible(true);
         }else if(Historia.parteDaHistoria==5){
-            /* Aqui poderia ser colocado um IF configurando botões separados
-             * para cada decisão da história, mas como ambos os casos
-             * apenas pedem um botão de continuar, isso foi omitido.
-             */
             textfield1.setVisible(false);
             button1.setText("Continuar");
             button2.setText("");
             button1.setVisible(true);
             button2.setVisible(false);
         }else if(Historia.parteDaHistoria==6){
-            textfield1.setVisible(false); //deve ter caixa de texto?
-            button1.setText("CAB"); //qual o texto do primeiro botão?
-            button2.setText("Reuni"); //qual o texto do segundo botão?
-            button1.setVisible(true); //deve ter primeiro botão?
-            button2.setVisible(true); //deve ter segundo botão?
-        }else{
+            textfield1.setVisible(false);
+            button1.setText("Continuar");
+            button2.setText("");
+            button1.setVisible(true);
+            button2.setVisible(false);
+        }else if(Historia.parteDaHistoria==7){
+            textfield1.setVisible(false);
+            button1.setText("CAB");
+            button2.setText("Reuni");
+            button1.setVisible(true);
+            button2.setVisible(true);
+        }else if(Historia.parteDaHistoria==8){
+            textfield1.setVisible(false);
+            button1.setText("104");
+            button2.setText("105");
+            button1.setVisible(true);
+            button2.setVisible(true);
+        }else if(Historia.parteDaHistoria==9){
+            textfield1.setVisible(false);
+            button1.setText("Examinar o local");
+            button2.setText("");
+            button1.setVisible(true);
+            button2.setVisible(false);
+        }else if(Historia.parteDaHistoria==10){
+            textfield1.setVisible(false);
+            button1.setText("Lutar!");
+            button2.setText("");
+            button1.setVisible(true);
+            button2.setVisible(false);
+        }else if(Historia.parteDaHistoria==11){
+            textfield1.setVisible(false);
+            button1.setText("Continuar");
+            button2.setText("");
+            button1.setVisible(true);
+            button2.setVisible(false);
+        }else if(Historia.parteDaHistoria==12){
+            textfield1.setVisible(false);
+            button1.setText("Continuar");
+            button2.setText("");
+            button1.setVisible(true);
+            button2.setVisible(false);
+        }else if(Historia.parteDaHistoria==13){
+            textfield1.setVisible(false);
+            button1.setText("Continuar");
+            button2.setText("");
+            button1.setVisible(true);
+            button2.setVisible(false);
+        }else if(Historia.parteDaHistoria==14){
+            textfield1.setVisible(false);
+            button1.setText("203");
+            button2.setText("204");
+            button1.setVisible(true);
+            button2.setVisible(true);
+        }else if(Historia.parteDaHistoria==15){
+            textfield1.setVisible(false);
+            button1.setText("Lutar!");
+            button2.setText("");
+            button1.setVisible(true);
+            button2.setVisible(false);
+        }else if(Historia.parteDaHistoria==16){
+            textfield1.setVisible(false);
+            button1.setText("Continuar");
+            button2.setText("");
+            button1.setVisible(true);
+            button2.setVisible(false);
+        }else if(Historia.parteDaHistoria==17){
+            textfield1.setVisible(false);
+            button1.setText("Lutar!");
+            button2.setText("");
+            button1.setVisible(true);
+            button2.setVisible(false);
+        }else if(Historia.parteDaHistoria==18){
+            textfield1.setVisible(false);
+            button1.setText("Continuar");
+            button2.setText("");
+            button1.setVisible(true);
+            button2.setVisible(false);
+        }else if(Historia.parteDaHistoria==19){
+            textfield1.setVisible(false);
+            button1.setText("Lutar!");
+            button2.setText("");
+            button1.setVisible(true);
+            button2.setVisible(false);
+        }
+        else{
             textfield1.setVisible(false);
             button1.setVisible(false);
             button2.setVisible(false);
@@ -125,43 +200,96 @@ public class PainelJogo extends JPanel{
                     if(Historia.parteDaHistoria==0){
                         InfoChar.nome=textfield1.getText(); //obter nome do personagem
                         textfield1.setText("");
+                        ClassesJogo.Historia.parteDaHistoria=1;
                     }else if(Historia.parteDaHistoria==1){
                         InfoChar.sexo="Masculino"; //homem
+                        ClassesJogo.Historia.parteDaHistoria=2;
                     }else if(Historia.parteDaHistoria==2){
                         InfoChar.classe=("Masculino".equals(InfoChar.sexo) ? "Paladino" : "Paladina"); //paladin
+                        ClassesJogo.Historia.parteDaHistoria=3;
                     }else if(Historia.parteDaHistoria==3){
                         InfoChar.forca+=5; //força
-                    }else if(Historia.parteDaHistoria==4){//luta
+                        ClassesJogo.Historia.parteDaHistoria=4;
+                    }else if(Historia.parteDaHistoria==4){
                         ProgramInit.entrarBatalha("Bando de Variáveis Desencapsuladas");
-                        Historia.decisao=1;
-                    }else if(Historia.parteDaHistoria==5){
-                        if(Historia.decisao==2){
-                            Historia.parteDaHistoria=4;
-                            ProgramInit.entrarBatalha("Bando de Variáveis Desencapsuladas");
-                            Historia.decisao=1;
-                        }
+                        ClassesJogo.Historia.parteDaHistoria=5;
+                    }else if(Historia.parteDaHistoria==5){//luta
+                        ClassesJogo.Historia.parteDaHistoria=7;
                     }else if(Historia.parteDaHistoria==6){
-                    }else{
+                        InfoChar.inteligencia+=2;
+                        ProgramInit.entrarBatalha("Bando de Variáveis Desencapsuladas");
+                        ClassesJogo.Historia.parteDaHistoria=5;
+                    }else if(Historia.parteDaHistoria==7){
+                        ClassesJogo.Historia.parteDaHistoria=8;
+                    }else if(Historia.parteDaHistoria==8){
+                        ClassesJogo.Historia.parteDaHistoria=12;
+                    }else if(Historia.parteDaHistoria==9){
+                        ClassesJogo.Historia.parteDaHistoria=10;
+                    }else if(Historia.parteDaHistoria==10){
+                        ClassesJogo.Historia.parteDaHistoria=11;
+                        ProgramInit.entrarBatalha("Ponteiro Selvagem");
+                    }else if(Historia.parteDaHistoria==11){
+                        ClassesJogo.Historia.parteDaHistoria=8;
+                    }else if(Historia.parteDaHistoria==12){
+                        ClassesJogo.Historia.parteDaHistoria=14;
+                    }else if(Historia.parteDaHistoria==13){
+                        InfoChar.forca+=3;
+                        ClassesJogo.Historia.parteDaHistoria=14;
+                    }else if(Historia.parteDaHistoria==14){
+                        ClassesJogo.Historia.parteDaHistoria=15;
+                    }else if(Historia.parteDaHistoria==15){
+                        ClassesJogo.Historia.parteDaHistoria=16;
+                        ProgramInit.entrarBatalha("Integral Perdida");
+                    }else if(Historia.parteDaHistoria==16){
+                        ClassesJogo.Historia.parteDaHistoria=17;
+                    }else if(Historia.parteDaHistoria==17){
+                        ClassesJogo.Historia.parteDaHistoria=18;
+                        ProgramInit.entrarBatalha("Robô Notebook");
+                    }else if(Historia.parteDaHistoria==18){
+                        ClassesJogo.Historia.parteDaHistoria=19;
+                    }else if(Historia.parteDaHistoria==19){
+                        ClassesJogo.Historia.parteDaHistoria=20;
+                        ProgramInit.entrarBatalha("Richard Stallman");
+                    }
+                    else{
                     } //após, exibe a próxima tela:
-                    ClassesJogo.Historia.parteDaHistoria++;
                     PainelChar.atualizar();
                     PainelJogo.atualizar();
                 }else if(event.getSource()==button2){ //Caso pressionado o segundo botão...
                     if(Historia.parteDaHistoria==0){ //não há segundo botão
+                        ClassesJogo.Historia.parteDaHistoria=1;
                     }else if(Historia.parteDaHistoria==1){
                         InfoChar.sexo="Feminino"; //mulher
+                        ClassesJogo.Historia.parteDaHistoria=2;
                     }else if(Historia.parteDaHistoria==2){
                         InfoChar.classe=("Masculino".equals(InfoChar.sexo) ? "Bruxo" : "Bruxa"); //witch
+                        ClassesJogo.Historia.parteDaHistoria=3;
                     }else if(Historia.parteDaHistoria==3){
                         InfoChar.inteligencia+=5; //inteligencia
-                    }else if(Historia.parteDaHistoria==4){ //professor
-                        InfoChar.inteligencia+=2;
-                        Historia.decisao=2;
+                        ClassesJogo.Historia.parteDaHistoria=4;              
+                    }else if(Historia.parteDaHistoria==4){
+                        ClassesJogo.Historia.parteDaHistoria=6;
                     }else if(Historia.parteDaHistoria==5){//não há segundo botão
-                    }else if(Historia.parteDaHistoria==6){
+                    }else if(Historia.parteDaHistoria==6){//não há segundo botão
+                    }else if(Historia.parteDaHistoria==7){
+                        ClassesJogo.Historia.parteDaHistoria=9;
+                    }else if(Historia.parteDaHistoria==8){
+                        ClassesJogo.Historia.parteDaHistoria=13;
+                    }else if(Historia.parteDaHistoria==9){//não há segundo botão
+                    }else if(Historia.parteDaHistoria==10){//não há segundo botão
+                    }else if(Historia.parteDaHistoria==11){//não há segundo botão
+                    }else if(Historia.parteDaHistoria==12){//não há segundo botão
+                    }else if(Historia.parteDaHistoria==13){//não há segundo botão
+                    }else if(Historia.parteDaHistoria==13){//não há segundo botão
+                    }else if(Historia.parteDaHistoria==14){
+                        ClassesJogo.Historia.parteDaHistoria=17;
+                    }else if(Historia.parteDaHistoria==15){//não há segundo botão
+                    }else if(Historia.parteDaHistoria==16){//não há segundo botão
+                    }else if(Historia.parteDaHistoria==17){//não há segundo botão
+                    }else if(Historia.parteDaHistoria==18){//não há segundo botão
+                    }else if(Historia.parteDaHistoria==19){//não há segundo botão
                     }else{
                     } //após, exibe a próxima tela:
-                    ClassesJogo.Historia.parteDaHistoria++;
                     PainelChar.atualizar();
                     PainelJogo.atualizar();
                 }
