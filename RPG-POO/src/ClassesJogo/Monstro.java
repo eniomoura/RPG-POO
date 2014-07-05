f4b5d8233d1d4148335211373cdb07bbfd2b49a9
Verde <Verde@Verde-PC>
2014-07-05 02:42:03 -0300

    Atualizado com os inimigos da história.
    Nem mexi nos stats de cada um, pois é apenas para teste.
    
    Balanceie sob suas preferências.

--- RPG-POO/src/ClassesJogo/Monstro.java
+++ RPG-POO/src/ClassesJogo/Monstro.java
@@ -13,9 +13,29 @@ public class Monstro{
     public Monstro(String Monstro){
         switch(Monstro) {
             case "Bando de Variáveis Desencapsuladas":
-                hp=50;
+                hp=7;
                 ataque=5;
                 defesa=6;
+                break;
+            case "Ponteiro Selvagem":
+                hp=5;
+                ataque=7;
+                defesa=3;
+                break;
+            case "Integral Perdida":
+                hp=5;
+                ataque=9;
+                defesa=2;
+                break;
+            case "Robô Notebook":
+                hp=5;
+                ataque=6;
+                defesa=4;
+                break;
+            case "Richard Stallman":
+                hp=5;
+                ataque=3;
+                defesa=5;
                 break;
             case "procGen":
                 throw new UnsupportedOperationException("Monstros procedurais não foram implementados.");
