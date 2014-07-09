089f08f9c21df3732abe1c1041713ca2e64faca0
Enio <Enio@192.168.1.51>
2014-07-09 20:39:37 -0300

    Projeto concluído e apresentado via EAD.

--- RPG-POO/src/GUIJogo/ProgramInit.java
+++ RPG-POO/src/GUIJogo/ProgramInit.java
@@ -7,26 +7,23 @@ public class ProgramInit{
 
     /*
      * Essa classe tem a finalidade de iniciar o aplicativo e a GUI.
-     * Desde a adição do sistema de combate, a main também tem algumas funções que requerem chamadas não estáticas
-     * da instância da janela principal, documentadas como "mágica". Evite tentar "consertar" isso,
-     * pode envolver a reescrita do programa inteiro.
      */
     public static MainFrame mainFrame=new MainFrame(); //Constrói a janela de jogo inicial
-    public static PainelBatalha batalha; //magic fuel
+    public static PainelBatalha batalha;
     
     public static void main(String[] args){ //Main. Herp Derp.
         mainFrame.setVisible(true); //Exibe janela de jogo principal.
     }
 
-    public static void entrarBatalha(String monster){ //white magic
+    public static void entrarBatalha(String monster){
         PainelMenu.salvar.setEnabled(false);
-        batalha=new PainelBatalha(monster); //magic motor
+        batalha=new PainelBatalha(monster);
         principal.setVisible(false);
         batalha.setVisible(true);
         mainFrame.add(batalha, BorderLayout.LINE_END);
     }
 
-    public static void sairBatalha(){ //dark magic
+    public static void sairBatalha(){
         PainelMenu.salvar.setEnabled(true);
         principal.setVisible(true);
         batalha.setVisible(false);
